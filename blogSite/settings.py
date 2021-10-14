904255edb63e9745849e34b8a05dd8b9b1f34c6a
lavish-jain <lavishrjain1997@gmail.com>
2021-10-15 02:40:17 +0530

    updated settings

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1#gw5t-5525j!)lme(+*+($!c-&_8uj56t$pgjnozu2(_u_f!l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_VALUE")
 
 ALLOWED_HOSTS = []
 
