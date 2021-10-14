d63a757f1dff7830b964a6823d3ab465a063d2bc
lavish-jain <lavishrjain1997@gmail.com>
2021-10-15 03:50:00 +0530

    changed settings

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -138,7 +138,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'users-login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
